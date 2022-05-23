4156862535a1afd34f99b9f3a038fde38eb1c81b
Ayden. S <49811848+TheRyshun@users.noreply.github.com>
2022-05-23 15:08:06 +0200

    Update client.js

--- js/client.js
+++ js/client.js
@@ -1,45 +1,66 @@
 var socket = io();
+var id_salon = 'salonG';
+var allMessages = [];
 
-socket.emit('set-pseudo',prompt("Pseudo ?"));
+socket.emit('set-pseudo', prompt("Pseudo ?"));
 
-var message = document.getElementById('messages');
+
+// Variables qui récupure éléments HTML
+var messages = document.getElementById('messages');
 var form = document.getElementById('form');
 var input = document.getElementById('input');
 
+
+// Écouteur et envoi du message du formulaire
 form.addEventListener('submit', (e) => {
-        e.preventDefault();
-        socket.emit('emission_message', input.value);
-        input.value="";
+    e.preventDefault();
+    if (input.value) {
+        socket.emit('emission_message', input.value, id_salon);
+        input.value = '';
+    }
 });
 
 
-socket.on('reception_message', (contenu) =>{
-
-        var li = document.createElement("li");
-        var val = document.createTextNode(contenu.pseudo + " : " + contenu.messages);
-        window.scrollTo(0, document.body.scrollHeight);
-        li.appendChild(val);
-        message.appendChild(li); 
-        console.log(contenu.pseudo + " : " + contenu.messages);
+// Reception ansi que l'affichage message
+socket.on('reception_message', (contenu) => {
+    allMessages.push({
+        pseudo: contenu.pseudo,
+        message: contenu.msg,
+        id_destinateur: contenu.id_destinateur,
+        id_emetteur: contenu.id_emetteur,
+        prive: contenu.prive
+    });
+    salon(id_salon);
+    check_unread();
 });
 
-socket.on('reception_utilisateur', (utilisateur) => {
-        pseudo.innerHTML = "";
-        var id_salon = 'salon';
-        var lesMessages = [];               
-        
-
-        utilisateur.forEach(contenu => {        
-        var li = document.createElement("li");
-        li.innerHTML = contenu.pseudo_client;
-        pseudo.appendChild(li);        
+// Affiche les utilisateurs de connéctés et channel public
+socket.on('reception_utilisateur', (contenu) => {
+    pseudo.innerHTML = "Channel :<br><br>";
+    pseudo.innerHTML += '<a href = # onClick = salon("salon")>Public</a><br><br>';
+    pseudo.innerHTML += "Utilisateurs connectés :<br><br>";
+    contenu.forEach(item => {
+        if (socket.id != item.id) {
+            pseudo.innerHTML += '<a href = # onClick = salon("' + item.id + '")><div>' + item.pseudo + '<span class ="badge badge-primary" id="' + item.id + '"></span></div></a>';
+        }
+        if (id_salon != 'salonG') {
+            document.getElementById(id_salon).innerHTML = "";
+        }
+    });
 });
 
+// La fonction salon permet d'afficher les messages d'un emetteur à son distinateur
+// ou dans le salon général
 function salon(id) {
-        console.log(id);
+    console.log(id);
+    id_salon = id;
+    messages.innerHTML = "";
+    allMessages.forEach((contenu) => {
+        if (contenu.id_destinateur == id_salon && id_salon == 'salonG' || contenu.id_destinateur == id_salon && contenu.id_emetteur == socket.id || contenu.id_destinateur == socket.id && contenu.id_emetteur == id_salon) {
+            var item = document.createElement('li');
+            item.textContent += contenu.pseudo + "  >>  " + contenu.message;
+            messages.appendChild(item);
+            window.scrollTo(-10, document.body.scrollHeight);
+        }
+    });
 }
-function check_unread() {
-        
-}
-})
-
